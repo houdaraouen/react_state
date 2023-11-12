4be7e5fa28cf7d5d71e283c6f509dde1b5f8726e
houdaraouen <raouenhouda6@gmail.com>
2023-11-12 09:59:25 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'Houda',
+      bio: 'A  developer',
+      imgSrc: 'https://media.istockphoto.com/id/546763388/fr/photo/le-point-de-vue-parfait.jpg?s=170667a&w=0&k=20&c=rG9LPeWfLJrAuafjVlL7WfAXMkEjoQshFgrxzA9ypE8=',
+      profession: 'Software Engineer',
+    },
+    show: false,
+    time: 0,
+  };
+
+  componentDidMount() {
+ setInterval(() => { this.setState(
+        (p) => ({ time: p.time + 1 }));
+    }, 1000);
+  }
+
+  Profile = () => {
+    this.setState((State) => ({
+      show: !State.show,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.Profile}>Profile</button>
+        {this.state.show && (
+          <div>
+            <h2>{this.state.person.fullName}</h2>
+            <p>{this.state.person.bio}</p>
+            <p>Profession: {this.state.person.profession}</p>
+            <img src={this.state.person.imgSrc} alt={this.state.person.fullName} />
+          </div>
+        )}
+        <p>Time : {this.state.time} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
